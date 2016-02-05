14914364166d7dae47458cc30964dfd45f528c85
shawn pivonka <sprov03@gmail.com>
2016-02-05 10:50:58 -0600

    Fixed the issue of skipping not skipping a lvl if it has been deleted;

--- app/controllers/LevelsController.php
+++ app/controllers/LevelsController.php
@@ -47,7 +47,8 @@ class LevelsController extends \BaseController {
 	 */
 	public function show($id)
 	{
-		$data = Call::where('level_id',$id)->get();
+		$data = Call::where('level_id', '>=', $id)->get();
+		// $data = Call::where('level_id',$id)->get();
 		return Response::json($data);
 	}
 
