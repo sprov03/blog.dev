9ce784383b447b69c9fe181f34f0da1ba956d37f
shawn pivonka <sprov03@gmail.com>
2016-05-27 08:20:00 -0500

    Made my first color picker

--- app/views/projects/googlemap.blade.php
+++ app/views/projects/googlemap.blade.php
@@ -1,48 +1,81 @@
 @extends('layouts.master')
 
 @section('top-script')
-	<style type="text/css">
-		#main{
-			width: 100%;
+	<style>
+		#geo_code{
+			position: relative;
+			top: 1px;
 		}
 		h1{
 			font-size: 20px;
 		}
 		#your_map{
-			margin: 0;
-			padding: 0;
-			width: 60vw;
+			box-sizing: border-box;
+			border-radius: 5px;
 			height: 60vw;
+		    margin: 0 15px 10px;
+		    padding: 0;
 		}
-
-		#demo{
-			/*height: 50px;*/
-			/*width: 50px;*/
+		#hue{
+			width: 100%; 
+			height: 35px;
+			border-radius:3px;
 		}
-
+		#controls{
+			text-align: center;
+		    margin: 0 15px 5px;
+		     padding: 0; 
+		}
+		.canvas{
+			/*position: static !important;*/
+		}
+		select, input, button{
+			margin: 0 0 5px 0;
+		}
 	</style>
 @stop
 
 
 
 @section('content')
-<div id="demo"></div>
+	<h1>Customize your map!</h1>
+	<div class="row">
 
-<h1>Customize your map!</h1>
-<div class="left-row">
-	<div id="your_map" class="col"></div>
-	<div class="col">
-		<input id="geo_address" placeholder="newadress"></input>
-		<button id="geo_code">geo</button>
-	</div>
-</div>
+		<div id="your_map" class="col-md-7">
+		</div>
+		
+		<div id="controls" class="col-md-5 canvas" >
+			<select class="form-control">
+				<option>Address</option>
+				<option>Hue</option>
+				<option>Lightness</option>
+				<option>Saturation</option>
+				<option>Invert Lightness</option>
+				<option>Visibility</option>
+				<option>Color</option>
+				<option>Weight</option>
+			</select>
+			<input id="geo_address" class="form-control" placeholder="new address"></input>
+			<canvas id="hue" width="100" height="100"></canvas>
+			<input id="lightness" class="form-control" type="number" placeholder="-100 to 100"></input>
+			<input id="saturation" class="form-control" type="number" placeholder="-100 to 100"></input>
+			<input id="gamma" class="form-control" type="number" placeholder=".01 t0 10"></input>
+			<input id="invert_lightness" class="form-control" type="number" placeholder="true or false"></input>
+			<input id="visibility" class="form-control" type="number" placeholder="(on, off, or simplified)"></input>
+			<input id="color" class="form-control" type="number" placeholder="(an RGB hex string)"></input>
+			<input id="weight" class="form-control" type="number" placeholder="(an integer value, greater than or equal to zero)"></input>
+			<button id="redraw" class="btn btn-default">redraw</button>
+		</div>
 
-
-
-
+	</div> 
 @stop
 
+
+
 @section('bottom-script')
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDe8A2JoXvvmNYaxz8Cs3eTJlzoQ12Kv7Q"></script>
 	<script>
 		// Create a map object, and include the MapTypeId to add to the map type control.
@@ -64,7 +97,7 @@
 		{
 			stylers: [
 				{ hue: "#d4e9ff" },
-				{ saturation: 60 }
+				{ saturation: 99 }
 			]
 		},{
 			featureType: "road",
@@ -97,47 +130,100 @@
 		}
 
 		   // Geocode our address
-		    document.getElementById('geo_code').addEventListener('click', function(){
+		    $('#geo_address').blur( function(){
 		    	geocoder.geocode({ "address": document.getElementById('geo_address').value }, function(results, status) {
 		           if (status == google.maps.GeocoderStatus.OK) {
 		               // map.setCenter(results[0].geometry.location);
 		               mapOptions.center.lat = results[0].geometry.location.lat();
 		               mapOptions.center.lng = results[0].geometry.location.lng();
-		               drawMap();
+		               // drawMap();
 		               for( var i=0; i<results.length; i++){
 		               	console.log( results[i].geometry.location.lat() );
 		               }
 		           } else {
 		               alert("Geocoding was not successful - STATUS: " + status);
 		           }
+		           console.log(mapOptions.center);
 		        });
 		    } );
 
-// User Interactions
-	drawMap();
-    var buttons = document.getElementsByTagName('a');
-    for(var i = 0; i < buttons.length;i+=1){
-        buttons[i].addEventListener('click',function () {
-            sorter(this.getAttribute('data-func'));
-        },false);
-    }
+		// User Interactions
+			drawMap();
+		    var buttons = document.getElementsByTagName('a');
+		    for(var i = 0; i < buttons.length;i+=1){
+		        buttons[i].addEventListener('click',function () {
+		            sorter(this.getAttribute('data-func'));
+		        },false);
+		    }
+		    $('#redraw').click(function(){
+		    	drawMap();
+		    });
+		    $('#saturation').change(function(){
+		    	var val = $(this).val();
+		    	if ( val > 100 ){
+		    		$(this).val(100);
+		    	}else if ( val < -100 ){
+		    		$(this).val(-100);
+		    	}else{
+		    		styles[0].stylers[0].hue = $(this).val();
+		    	}
+		    });
 
 	</script>
 
-	<script>
-var x = document.getElementById("demo");
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
-}
-getLocation(); 
+
+<script type="text/javascript">
+	var canvas = document.getElementById('hue').getContext('2d');
+
+	// create an image object and get it’s source
+	var img = new Image();
+	img.src = '/img/color-picker.png';
+
+	// copy the image to the canvas
+	$(img).load(function(){
+	  var sx = 0;
+	  var sy = 100;
+	  var sw = 250;
+	  var sh = 20;
+	  var cx = 0;
+	  var cy = 0;
+	  var cw = 100;
+	  var ch = 100;
+	  canvas.drawImage(img,sx,sy,sw,sh,cx,cy,cw,ch);
+	});
+
+	// http://www.javascripter.net/faq/rgbtohex.htm
+	function rgbToHex(R,G,B) {return toHex(R)+toHex(G)+toHex(B)}
+	function toHex(n) {
+	  n = parseInt(n,10);
+	  if (isNaN(n)) return "00";
+	  n = Math.max(0,Math.min(n,255));
+	  return "0123456789ABCDEF".charAt((n-n%16)/16)  + "0123456789ABCDEF".charAt(n%16);
+	}
+	$('#hue').click(function(event){
+	  // getting user coordinates
+	  var x = (event.pageX - this.offsetLeft) / $('#hue').width() * 100;
+	  var y = (event.pageY - this.offsetTop) / $('#hue').width() * 100;
+	  // getting image data and RGB values
+	  var img_data = canvas.getImageData(x, y, 1, 1).data;
+	  var R = img_data[0];
+	  var G = img_data[1];
+	  var B = img_data[2];  
+	  var rgb = R + ',' + G + ',' + B;
+	  // convert RGB to HEX
+	  var hex = rgbToHex(R,G,B);
+	  // making the color the value of the input
+	  $('#rgb input').val(rgb);
+	  $('#hex input').val('#' + hex);
+
+	  styles[0].stylers[0].hue = '#' + hex;
+	  drawMap();
+	});
 </script>
+
+
+
+
+
 
 @stop
